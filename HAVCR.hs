5305220864bda55eb0a71a0bc9e81002db43d926
Slava Kravchenko <cordawyn@neptune.(none)>
2012-10-19 10:09:49 +0300

    proper parsing of request headers

--- HAVCR.hs
+++ HAVCR.hs
@@ -10,28 +10,45 @@ import Network.URI
 import Data.Text
 import Data.Maybe (fromJust)
 import Data.HashMap.Strict (toList)
+import qualified Data.Vector as V
 
 instance FromJSON URI where
     parseJSON (String u) = pure $ fromJust $ parseURI $ unpack u
+    parseJSON _ = fail "could not parse"
 
 instance FromJSON RequestMethod where
     parseJSON (String m) = pure GET -- TODO
                                     -- Y U NO EXPORT "rqMethodMap"???
                                     -- pure $ Prelude.lookup (unpack m) rqMethodMap
+    parseJSON _ = fail "could not parse"
 
-instance FromJSON Header where
-    parseJSON (Object h) = Header <$>
-                           pure hName <*>
-                           parseJSON v
-                           where (k, v) = Prelude.head $ toList h
-                                 hName = fromJust $ Prelude.lookup (unpack k) headerMap
+-- instance FromJSON Header where
+--     parseJSON (Object h) = Header <$>
+--                            pure hName <*>
+--                            parseJSON v
+--                            where (k, v) = Prelude.head $ toList h
+--                                  hName = fromJust $ Prelude.lookup (unpack k) headerMap
+--     parseJSON _ = fail "could not parse"
 
 instance FromJSON r => FromJSON (Request r) where
     parseJSON (Object v) = Request <$>
                            v .: "uri" <*>
                            v .: "method" <*>
-                           v .: "headers" <*>
+                           -- remap {a: [1, 2], b: [3]} to [(a, 1), (a, 2), (b, 3)]
+                           (fmap mkHeaders $ v .: "headers") <*>
                            v .: "body"
+    parseJSON _ = fail "could not parse"
+
+mkHeaders :: Value -> [Header]
+mkHeaders (Object hs) = Prelude.concat $ Prelude.map hNameToHdrs $ toList hs
+    where hNameToHdrs (k, Array vs) = Prelude.map (hNameToHdr k) (V.toList vs)
+          hNameToHdr k (String v) = mkHeader (fromJust $ Prelude.lookup (unpack k) headerMap) (unpack v)
+
+mkHeaders2 :: (Text, [String]) -> [Header]
+mkHeaders2 (k, hs) = Prelude.map hNameToHdr hs
+    where hNameToHdr = mkHeader (fromJust $ Prelude.lookup (unpack k) headerMap)
+
+--------------------------------------
 
 instance ToJSON URI where
     toJSON uri = String $ pack $ uriToString id uri ""
@@ -39,8 +56,9 @@ instance ToJSON URI where
 instance ToJSON RequestMethod where
     toJSON meth = String $ pack $ show meth
 
-instance ToJSON Header where
-    toJSON (Header hName hValue) = object [(pack $ show hName) .= hValue]
+instance ToJSON Header where -- TODO
+    toJSON (Header hName hValue) = object []
+    -- toJSON (Header hName hValue) = object [(pack $ show hName) .= hValue]
 
 instance ToJSON r => ToJSON (Request r) where
     toJSON (Request uri meth heads body) =
--- test/HAVCRTests.hs
+++ test/HAVCRTests.hs
@@ -1,11 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Test.HUnit
 import Test.Framework
+import Test.Framework.Providers.HUnit
 
-import HAVCR.ProxyTest
+import HAVCR
+import Network.HTTP
+import Network.URI
+import Data.Text
+import Data.Yaml
+import Data.Maybe (fromJust)
 
 main :: IO ()
 main =
      do ropts <- interpretArgsOrExit []
         defaultMainWithOpts tests ropts
-        where tests = HAVCR.ProxyTest.tests
+
+tests = [ testCase "1" test_parseRequest ]
+
+test_parseRequest =
+  do req <- decodeFile "test/fixtures/sample02.yml" :: IO (Maybe (Request Text))
+     assertEqual "URI" (fromJust $ parseURI "http://example.com/result?a=true&b=0") (rqURI (fromJust req))
+     assertEqual "method" "GET" (show $ rqMethod (fromJust req))
+     assertEqual "headers" expectedHeaders (Prelude.map (\h -> show (hdrName h) ++ ": " ++ hdrValue h) $ rqHeaders (fromJust req))
+     assertEqual "body" "HELLO" (rqBody (fromJust req))
+     where expectedHeaders = [ "Content-Type: application/x-www-form-urlencoded",
+                               "Accept: */*",
+                               "User-Agent: Ruby",
+                               "User-Agent: Haskell" ]
