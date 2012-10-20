dbc2095ca796314233e6d83e0cb0aa3176d220c1
Slava Kravchenko <cordawyn@neptune.(none)>
2012-10-20 18:00:49 +0300

    Request serialization test

--- test/HAVCRTests.hs
+++ test/HAVCRTests.hs
@@ -10,6 +10,7 @@ import HAVCR
 import Network.HTTP
 import Network.URI
 import Data.Text
+import Data.Text.Encoding
 import Data.Yaml
 import Data.Maybe (fromJust)
 
@@ -18,15 +19,27 @@ main =
      do ropts <- interpretArgsOrExit []
         defaultMainWithOpts tests ropts
 
-tests = [ testCase "1" test_parseRequest ]
+tests = [ testCase "parse Request" test_parseRequest
+          , testCase "serialize Request" test_serializeRequest
+        ]
 
 test_parseRequest =
   do req <- decodeFile "test/fixtures/sample02.yml" :: IO (Maybe (Request Text))
      assertEqual "URI" (fromJust $ parseURI "http://example.com/result?a=true&b=0") (rqURI (fromJust req))
-     assertEqual "method" "GET" (show $ rqMethod (fromJust req))
+     assertEqual "method" "POST" (show $ rqMethod (fromJust req))
      assertEqual "headers" expectedHeaders (Prelude.map (\h -> show (hdrName h) ++ ": " ++ hdrValue h) $ rqHeaders (fromJust req))
      assertEqual "body" "HELLO" (rqBody (fromJust req))
      where expectedHeaders = [ "Content-Type: application/x-www-form-urlencoded",
                                "Accept: */*",
                                "User-Agent: Ruby",
                                "User-Agent: Haskell" ]
+
+test_serializeRequest =
+  let req = Request (fromJust $ parseURI "http://example.com/result?a=true&b=0") POST headers ("HELLO" :: Text)
+      headers = [   Header HdrContentType "application/x-www-form-urlencoded"
+                  , Header HdrAccept "*/*"
+                  , Header HdrUserAgent "Ruby"
+                  , Header HdrUserAgent "Haskell"
+                  ]
+  in do yaml <- readFile "test/fixtures/sample02.yml"
+        assertEqual "YAML" (encodeUtf8 $ pack yaml) (encode req)
