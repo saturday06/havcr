dcb6893421023103451f2f106dca2af2b1ddd223
Slava Kravchenko <cordawyn@neptune.(none)>
2012-12-27 21:22:59 +0200

    HAVCR proxy, first approximation

---
+++ Network/HAVCR/Proxy.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+-- HAVCR module providing a "simulated server" for testing
+module Network.HAVCR.Proxy (
+  mockedServer,
+  simulatedHTTP
+) where
+
+import Control.Proxy
+import Control.Monad
+import Network.HTTP
+import Network.Stream
+import Network.BufferType
+import Data.Text
+import Data.String (IsString)
+
+mockedServer :: forall ty p. (HStream ty, IsString ty, Proxy p) => Request ty -> Server p (Request ty) (Result (Response ty)) IO (Request ty)
+mockedServer = runIdentityK $ foreverK $ \req ->
+    -- do result <- lift $ simpleHTTP req
+    do result <- lift $ simulatedHTTP req
+       respond result
+
+simulatedHTTP :: forall ty. (HStream ty, IsString ty) => Request ty -> IO (Network.Stream.Result (Response ty))
+simulatedHTTP req = return $ Right $ Response (2,0,0) "OK" [] ("hello" :: ty)
--- test/Network/HAVCR/ProxyTest.hs
+++ test/Network/HAVCR/ProxyTest.hs
@@ -1,5 +1,7 @@
 module HAVCR.ProxyTest where
 
+import Network.HAVCR.Proxy
+
 import Test.HUnit
 import Test.Framework (testGroup)
 import Test.Framework.Providers.HUnit (testCase)
