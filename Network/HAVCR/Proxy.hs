faf8ec64039d87a4597349e8f5a17e216d46e3d5
Slava Kravchenko <cordawyn@neptune.(none)>
2013-01-05 00:26:03 +0300

    mockedServer returns a polymorphic value

--- Network/HAVCR/Proxy.hs
+++ Network/HAVCR/Proxy.hs
@@ -13,11 +13,11 @@ import Network.BufferType
 import Data.Text
 import Data.String (IsString)
 
-mockedServer :: forall ty p. (HStream ty, IsString ty, Proxy p) => Request ty -> Server p (Request ty) (Result (Response ty)) IO (Request ty)
+mockedServer :: forall ty p r. (HStream ty, IsString ty, Proxy p) => Request ty -> Server p (Request ty) (Result (Response ty)) IO r
 mockedServer = runIdentityK $ foreverK $ \req ->
     -- do result <- lift $ simpleHTTP req
     do result <- lift $ simulatedHTTP req
        respond result
 
-simulatedHTTP :: forall ty. (HStream ty, IsString ty) => Request ty -> IO (Network.Stream.Result (Response ty))
+simulatedHTTP :: forall ty. (HStream ty, IsString ty) => Request ty -> IO (Result (Response ty))
 simulatedHTTP req = return $ Right $ Response (2,0,0) "OK" [] ("hello" :: ty)
