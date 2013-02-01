b5e5af200420666d6a66cbc2784244d11b8b3bbd
Akira Shirase <slava.kravchenko@gmail.com>
2013-02-01 22:53:22 +0300

    fixed the test

--- test/Network/HAVCR/ProxyTest.hs
+++ test/Network/HAVCR/ProxyTest.hs
@@ -27,13 +27,27 @@ testClient :: forall ty p r. (HStream ty, IsString ty, Proxy p) =>
               () -> Client p (Request String) (Result (Response ty)) IO (Result (Response ty))
 testClient () = runIdentityP $ do
     request req
-    where req = Request (fromJust $ parseURI "http://www.example.com") GET [] "test" :: Request String
+    where req = Request (fromJust $ parseURI testURI) testMethod testHeaders testBody :: Request String
+          testURI = "http://example.com/result?a=true&b=0"
+          testMethod = POST
+          testHeaders = [
+            Header HdrContentType "application/x-www-form-urlencoded",
+            Header HdrAccept "*/*",
+            Header HdrUserAgent "Ruby"
+            ]
+          testBody = "HELLO"
 
 testProxy :: Proxy p => Cassette String -> () -> Session p IO (Result (Response String))
 testProxy cas = mockedServer cas >-> testClient
 
 test_SimpleMockedResponse = do
-  cas <- decodeFile "cassette.yml"
+  cas <- decodeFile "test/fixtures/sample01.yml"
   actualResponse <- runProxy $ testProxy $ fromJust cas :: IO (Result (Response String))
   assertEqual "response" (Right $ expectedResponse) actualResponse
-    where expectedResponse = Response (2,0,0) "OK" [] "hello" :: Response String
+    where expectedResponse = Response (2,0,0) "OK" headers "RESULT" :: Response String
+          headers = [
+            Header HdrContentType "text/plain; charset=iso-8859-2",
+            Header HdrServer "Apache",
+            Header HdrDate "Wed, 28 Dec 2011 15:08:44 GMT",
+            Header HdrContentLength "12"
+            ]
